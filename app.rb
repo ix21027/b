d6884ae6f6c67e559f3617430a3a1bbb4b2a126e
IX <khyzhniak.i@outlook.com>
2021-08-24 22:52:01 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,30 +2,30 @@ require 'telegram/bot'
 require 'nokogiri'
 require 'open-uri'
 
-token = '1896440784:AAFf9SNej12QWZ4gvHbq5ZlvCwkIWIYABE8'
+token = ENV['TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
   	begin
     	case message
     	when Telegram::Bot::Types::Message
-    		case message.text
-    		when '/start'
-    			btns = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: %w[today dream], one_time_keyboard: true)
-    			bot.api.send_message(chat_id: message.chat.id, text: 'helo', reply_markup: btns)
-    		when '/today'
-      		m = Nokogiri::HTML(URI.open('https://rivendel.ru/today.php?idr=7')).at(".today")
+    	  case message.text
+    	  when '/start'
+    	    btns = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: %w[today dream], one_time_keyboard: true)
+    	    bot.api.send_message(chat_id: message.chat.id, text: 'helo', reply_markup: btns)
+    	  when '/today'
+      	    m = Nokogiri::HTML(URI.open('https://rivendel.ru/today.php?idr=7')).at(".today")
   				useless = %w[//script //a[contains(.,'>>')]  //a[contains(.,'<<')] ins #region_select_switcher]
   				m.search(useless).map { _1.remove }
   				m.search('br').map {_1.content="\n"}
   				doc = m.text.split("\n").map {_1.strip }.filter {!_1.empty?}.join("\n").scan /.{1,4096}/m
   				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1.gsub("\n    ", '')) }
-    		when '/dream'
-      		m = Nokogiri::HTML(URI.open('https://rivendel.ru/dream_lenta.php?idr=7')).at('.workarea')
+    	  when '/dream'
+      	    m = Nokogiri::HTML(URI.open('https://rivendel.ru/dream_lenta.php?idr=7')).at('.workarea')
   				m.search('//script').map { _1.remove }
   				doc = m.text.scan /.{1,4096}/m
   				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1) }
-    		end
+    	  end
     	end
     rescue => e
       p e
