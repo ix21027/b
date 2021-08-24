96c9839bbfff3579a4ecf3437b3b47c74462e139
IX <khyzhniak.i@outlook.com>
2021-08-25 01:37:54 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -17,14 +17,13 @@ Telegram::Bot::Client.run(token) do |bot|
   				useless = %w[//script //a[contains(.,'>>')]  //a[contains(.,'<<')] ins #region_select_switcher]
   				m.search(*useless).map { _1.remove }
   				m.search('br').map {_1.content="\n"}
-  				doc = m.text.squeeze(" ").scan /.{1,4096}/m
+  			        doc = m.text.split("\n").map {_1.strip }.filter {!_1.empty?}.join("\n").squeeze(" ").scan /.{1,4096}/m
   				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1.gsub("\n    ", '')) }
     	  when '/dream'
       	    m = Nokogiri::HTML(URI.open('https://rivendel.ru/dream_lenta.php?idr=7')).at('.workarea')
   				m.search('//script').map { _1.remove }
                                 m.search(%w[ins #region_select_switcher]).map { _1.remove }
-                                m.split("\n").shift;m.join("\n").squeeze(" ")
-  				doc = m.text.scan /(.{1,3247})/m
+                                doc = m.text.split("\n").shift;m.join("\n").squeeze(" ").scan /(.{1,3247})/m
   			        bot.api.send_message(chat_id: message.chat.id, text: doc)
     	  end
     	end
