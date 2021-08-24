284a90fea7e086e4ffdc68ff8ce5560536db023a
IX <khyzhniak.i@outlook.com>
2021-08-24 23:28:44 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ Telegram::Bot::Client.run(token) do |bot|
     	  when '/today'
       	    m = Nokogiri::HTML(URI.open('https://rivendel.ru/today.php?idr=7')).at(".today")
   				useless = %w[//script //a[contains(.,'>>')]  //a[contains(.,'<<')] ins #region_select_switcher]
-  				m.search(useless).map { _1.remove }
+  				m.search(*useless).map { _1.remove }
   				m.search('br').map {_1.content="\n"}
   				doc = m.text.split("\n").map {_1.strip }.filter {!_1.empty?}.join("\n").scan /.{1,4096}/m
   				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1.gsub("\n    ", '')) }
