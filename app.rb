cba2890ab8d7dacd87baec36258b682e41f9fff7
IX <khyzhniak.i@outlook.com>
2021-08-25 15:48:19 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ Telegram::Bot::Client.run(token) do |bot|
             m.search('//script').map { _1.remove }
             m.search(%w[ins #region_select_switcher]).map { _1.remove }        
             doc = m.text.split("\n").uniq.drop(4).join("\n").scan(/(.{1,4096})/m).flatten[0].split("\n                ").shift(6).join.split("\n")[1..-6]
-            bot.api.send_message(chat_id: message.chat.id, text: doc[0..5].join("\n").gsub(/\sЗначение снов в .{1,4} лунный день/, "\n")) 
-            bot.api.send_message(chat_id: message.chat.id, text: doc[6..-1].join("\n").gsub(/\sЗначение снов в .{1,4} лунный день/, "\n"))
+            bot.api.send_message(chat_id: message.chat.id, text: doc[0..5].join("\n").gsub(/Значение снов в .{1,4} лунный день/, "")) 
+            bot.api.send_message(chat_id: message.chat.id, text: doc[6..-1].join("\n").gsub(/Значение снов в .{1,4} лунный день/, ""))
     	  end
     	end
     rescue => e
