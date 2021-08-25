7273366a79ba89d796f34a470666d5a6aef6f0c2
IX <khyzhniak.i@outlook.com>
2021-08-25 14:23:52 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -18,14 +18,14 @@ Telegram::Bot::Client.run(token) do |bot|
   				m.search(*useless).map { _1.remove }
   				m.search('br').map {_1.content="\n"}
   			        doc = m.text.split("\n").map {_1.strip }.filter {!_1.empty?}[1..-1].join("\n").squeeze(" ").scan /.{1,4096}/m
-  				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1.gsub("\n    ", '').gsub(/(Значение снов в).{1,4}(лунный день)/, "\n")) }
+  				doc.map { bot.api.send_message(chat_id: message.chat.id, text: _1.gsub("\n    ", '') }
     	  when '/dream'
       	    m = Nokogiri::HTML(URI.open('https://rivendel.ru/dream_lenta.php?idr=7')).at('.workarea')
             m.search('//script').map { _1.remove }
             m.search(%w[ins #region_select_switcher]).map { _1.remove }        
             doc = m.text.split("\n").uniq.drop(4).join("\n").scan(/(.{1,4096})/m).flatten[0].split("\n                ").shift(6).join.split("\n")[1..-6]
-            bot.api.send_message(chat_id: message.chat.id, text: doc[0..5].join("\n"))
-            bot.api.send_message(chat_id: message.chat.id, text: doc[6..-1].join("\n"))
+            bot.api.send_message(chat_id: message.chat.id, text: doc[0..5].join("\n").gsub(/(Значение снов в).{1,4}(лунный день)/, "\n")) 
+            bot.api.send_message(chat_id: message.chat.id, text: doc[6..-1].join("\n").gsub(/(Значение снов в).{1,4}(лунный день)/, "\n"))
     	  end
     	end
     rescue => e
