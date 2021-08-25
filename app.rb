e4f35365a76fd428799a6977ee7f4d0ffb0e1d0d
IX <khyzhniak.i@outlook.com>
2021-08-25 12:13:38 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ Telegram::Bot::Client.run(token) do |bot|
             m.search('//script').map { _1.remove }
             m.search(%w[ins #region_select_switcher]).map { _1.remove }
             
-            doc = m.text.split("\n").uniq.drop(1).join("\n").scan(/(.{1,4096})/m).flatten
+            doc = m.text.split("\n").drop(1).uniq.join("\n").scan(/(.{1,4096})/m).flatten
             bot.api.send_message(chat_id: message.chat.id, text: doc[0])
     	  end
     	end
